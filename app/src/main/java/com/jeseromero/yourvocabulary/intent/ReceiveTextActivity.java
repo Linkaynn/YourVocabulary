1871577e41bb880f623a307fb6055d315799077f
Jesé Romero Arbelo <jeseromeroarbelo>
2017-03-11 12:18:59 +0100

    First commit

---
+++ app/src/main/java/com/jeseromero/yourvocabulary/intent/ReceiveTextActivity.java
@@ -0,0 +1,39 @@
+package com.jeseromero.yourvocabulary.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.jeseromero.yourvocabulary.R;
+
+public class ReceiveTextActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setTitle("New word");
+
+		setContentView(R.layout.activity_recieve_text);
+
+
+		Intent intent = getIntent();
+		String action = intent.getAction();
+		String type = intent.getType();
+
+		if (Intent.ACTION_SEND.equals(action) && type != null) {
+			if ("text/plain".equals(type)) {
+				handleSendText(intent);
+			}
+		} else {
+		}
+	}
+
+	private void handleSendText(Intent intent) {
+		String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+		if (sharedText != null) {
+			Toast.makeText(this, sharedText, Toast.LENGTH_LONG).show();
+		}
+	}
+}
